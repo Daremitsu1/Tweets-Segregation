a2e84644375792c3ce4cb27e19e19371ffbbcf2c
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-01-08 15:50:07 +0530

    Update map-reduce.py

--- map-reduce.py
+++ map-reduce.py
@@ -1,8 +1,18 @@
 # Mapper function
 def map(key, value):
-    user = value.split(',')[0]  # Get the user from the tweet
-    yield user, 1
+    # Parse the tweet JSON
+    tweet = json.loads(value)
+    # Extract the tweet text, user name, and number of followers
+    text = tweet['text']
+    user = tweet['user']['screen_name']
+    followers = tweet['user']['followers_count']
+    # Yield the extracted values as a key-value pair
+    yield (user, followers), text
 
 # Reducer function
 def reduce(key, values):
-    yield key, sum(values)
+    # Concatenate the tweet texts into a single string
+    texts = ' '.join(values)
+    # Yield the user name, number of followers, and concatenated tweet text as a CSV string
+    yield ','.join(map(str, key)) + ',' + texts
+
